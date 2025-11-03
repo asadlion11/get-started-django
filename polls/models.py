1bca2e8194c7dc72b4dd75fd75267c235e4c9ab6
asadlion11 <mabdiaziz416@gmail.com>
2025-11-03 21:37:08 +0300

    added new concepts

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+import  datetime
+from django.utils import timezone
 
 # Create your models here.
+# we’ll create two models: Question and Choice. A Question has a question and a publication date. A Choice has two fields: the text of the choice and a vote tally. Each Choice is associated with a Question.
+
+# Question Model(2 fields question and publication date)
+class Question(models.Model):
+    # class variables = database field(column)
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("data published") 
+    # pub_date: machine readable name while data published: human readable name called verbose name(optional)
+    # If you don’t supply one, Django auto-generates a readable name
+    def __str__(self):
+        return self.question_text
+    
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+# Choice Model(3 fields: question foreign key(Question Mddel) choice text, votes, and )
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE) # in the db table = question_id integer NOT NULL, stores the Question PK
+    # ForeignKey = Choice FK field mapped to Question PK(auto genetare if you did not add the models field) field  
+    # ForeignKey. That tells Django each Choice is related to a single Question. 
+    # on_delete matters: #CASCADE deletes Choices if the Question is deleted.
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.choice_text
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello wordl from polls app")
+    # order the Question objects by pub_date descending and get the first 5 questions
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    developer_name = "Moahmed"
+    # loop through the latest_question_list and each question is initialized to q and get the question_text attribute then seperate questions by comma
+    
+    # The context is a dictionary mapping template variable names to Python objects.
+    context = {
+        "latest_question_list": latest_question_list,
+        "developer_name": developer_name,
+        }
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    return render(request, "polls/index.html", context)
+    # render: load template, fill context, return HttpResponse object
+    # It returns an HttpResponse object of the given template rendered with the given context.
+
+def detail(request, question_id):
+    return HttpResponse(f"You are looking the detail of the question {question_id}")
+
+def results(request, question_id):
+    return HttpResponse(f"You are looking the result of the question {question_id}")
+
+def vote(request, question_id):
+    return HttpResponse(f"You are voting the question {question_id}")
